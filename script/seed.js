2562d5c9c618757fbaa8bef436a565151d6c2574
Jonathan Martinez <JonM90@gmail.com>
2017-11-21 21:12:28 -0500

    Update seed.js

--- script/seed.js
+++ script/seed.js
@@ -19,27 +19,13 @@ async function seed () {
   // executed until that promise resolves!
 
   const users = await Promise.all([
-<<<<<<< HEAD
-    User.create({email: 'Jack@email.com', password: '123', name: 'Samurai Jack', isAdmin: false, rank: 1, points: 5}),
-    User.create({email: 'Jonathan@email.com', password: '123', name: 'Jonathan', isAdmin: false, rank: 1, points: 0})
-=======
     User.create({name: 'Samurai Jack', email: 'Jack@email.com', password: '123', rank: 1, score: 0, isAdmin: false}),
     User.create({name: 'Jonathan', email: 'Jonathan@email.com', password: '123', rank: 2, score: 0, isAdmin: false})
->>>>>>> master
   ])
 //CHANGE REST OF TESTSPECS TO MATCH BOTTOM ONES
   const problems = await Promise.all([
     Problem.create({title: 'Sum', level: 1, description: 'Return the sum of two numbers', solution: '(a,b)=> a+b', testSpecs: ['sum(1,2) === 3', 'sum(2,2) === 4'], authorId: 1}),
     Problem.create({title: 'Diff', level: 1, description: 'Return the difference of two numbers', solution: '(a,b)=> a-b', testSpecs: ['diff(1,2) === -1', 'diff(2,1) === 1'], authorId: 2}),
-<<<<<<< HEAD
-    Problem.create({title: 'FizzBuzz', level: 1, description: 'Write a function that accepts a single number as an argument. Return “Fizz” for any numbers that are divisible by 3, “Buzz” for any numbers that are divisible by 5, and “FizzBuzz” for any numbers divisible by both 3 and 5. Else, return false', solution: `function fizzBuzz(num){
-        if (num%15 === 0) {
-          return “FizzBuzz”;
-        } else if (num%5 === 0) {
-          return “Buzz”;
-        } else if (num%3 === 0) {
-          return “Fizz”;
-=======
     Problem.create({title: 'FizzBuzz', level: 2, description: 'Write a function that accepts a single number as an argument. Return "Fizz" for any numbers that are divisible by 3, "Buzz" for any numbers that are divisible by 5, and "FizzBuzz" for any numbers divisible by both 3 and 5. Else, return false', solution: `function fizzBuzz(num){
         if (num%15 === 0) {
           return "FizzBuzz";
@@ -47,21 +33,14 @@ async function seed () {
           return "Buzz";
         } else if (num%3 === 0) {
           return "Fizz";
->>>>>>> master
         } else {
           return false;
         }
       }
     `, testSpecs: ['FizzBuzz(15) === "FizzBuzz"', 'FizzBuzz(3) === "Buzz"', 'FizzBuzz(5) === "Fizz"','FizzBuzz(12) === "Fizz"' ], authorId: 1}),
-<<<<<<< HEAD
-    Problem.create({title: 'bactrianCase', level: 1, description: 'write a function bactrianCase that accepts a single string as an argument. The function should log out that string with every other letter capitalized.', solution: `function bactrianCase(str) {
-      var newString = '';
-    
-=======
     Problem.create({title: 'bactrianCase', level: 3, description: 'write a function bactrianCase that accepts a single string as an argument. The function should log out that string with every other letter capitalized.', solution: `function bactrianCase(str) {
       var newString = '';
 
->>>>>>> master
       for(var i=0; i<str.length; i++) {
         if (i%2 === 0) {
           newString += str[i].toUpperCase();
@@ -69,26 +48,6 @@ async function seed () {
           newString += str[i];
         }
       }
-<<<<<<< HEAD
-    
-      return newString;
-    }`, testSpecs: ['bactrianCase("hello") === "HeLlO"', 'bactrianCase("stephanie") === "StEpHaNiE"', 'bactrianCase("fullstack") === "FuLlStAcK"'], authorId: 2}),
-    Problem.create({title: 'exponentiate', level: 1, description: 'Write a function exponentiate that accepts a number and a power to raise that number to. For the present, assume the power argument will always be a positive integer value.', solution: `function exponentiate(base, power) {
-      var expResult = 1;
-    
-     for (var i=0; i<power; i++) {
-        expResult *= base;
-      }
-    
-     return expResult;
-    }`, testSpecs: ['assert.equal(exponentiate(1,1), 1)', 'assert.equal(exponentiate(3,3), 27)', 'assert.equal(exponentiate(6,7), 279936)' ], authorId: 2}),
-    Problem.create({title: 'mostVowels', level: 1, description: 'Write a function that accepts a string and returns the word from that string with the most vowels. If there are no words with strings, return the empty string.', solution: `function mostVowels(str) {
-      var vowels = “aeiou”;
-      var wordsArr = str.split(” “);
-      var leadWord = “”;
-      var mostVowels = 0;
-    
-=======
 
       return newString;
     }`, testSpecs: ['bactrianCase("hello") === "HeLlO"', 'bactrianCase("stephanie") === "StEpHaNiE"', 'bactrianCase("fullstack") === "FuLlStAcK"'], authorId: 2}),
@@ -107,7 +66,6 @@ async function seed () {
       var leadWord = "";
       var mostVowels = 0;
 
->>>>>>> master
      for (var i=0; i<wordsArr.length; i++) {
         var word = wordsArr[i];
         var vowelCount = 0;
@@ -121,18 +79,10 @@ async function seed () {
           leadWord = word;
         }
       }
-<<<<<<< HEAD
-    
-     return leadWord;
-    }`, testSpecs: ['assert.equal(mostVowels("I am a keeper with some real rhythms"), "keeper")', 'assert.equal(mostVowels("Tenacious Tony Trains Today, Tomorrow Till The Time Ticks"), "Tenacious")'], authorId: 1})
-    
-    ])
-=======
 
      return leadWord;
     }`, testSpecs: ['assert.equal(mostVowels("I am a keeper with some real rhythms"), "keeper")', 'assert.equal(mostVowels("Tenacious Tony Trains Today, Tomorrow Till The Time Ticks"), "Tenacious")'], authorId: 1})
   ])
->>>>>>> master
 
   const complete = await Promise.all([
     CompletedProblem.create({rating: 4, userId: 1, problemId: 2}),
