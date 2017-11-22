32e771dd27867f3148a5bb183db5048fd8e879b2
Amadou Jallow <wuri1@live.com>
2017-11-22 12:47:32 -0500

    modified pop-up window

--- client/components/pop_up.js
+++ client/components/pop_up.js
@@ -7,7 +7,48 @@ export class PopUp extends Component{
    }
 
   componentDidMount(){
-    Popup.alert('Hello, look at me')
+    Popup.create({
+      title: null,
+      content: 'This popup uses the create method directly to get more control. This popup demonstrates custom buttons.',
+      buttons: {
+          left: [{
+              text: 'Cancel',
+              className: 'danger',
+              key: 'esc',
+              action: function () {
+                  Popup.alert('You pressed the Cancel btn');
+  
+                  /** Close this popup. Close will always close the current visible one, if one is visible */
+                  Popup.close();
+              }
+          }],
+          right: [{
+              text: 'Alt',
+              key: '⌘+enter',
+              action: function () {
+                  // Passing true as the second argument to the create method
+                  // displays it directly, without interupting the queue.
+                  Popup.create({
+                      title: null,
+                      content: 'I was configured to display right away, without affecting the queue. Closing this will display the previously visible popup.',
+                      buttons: {
+                          left: ['cancel'],
+                          right: []
+                      }
+                  }, true);
+              }
+          }, {
+              text: 'Save',
+              className: 'success',
+              action: function () {
+                  Popup.alert('You pressed the Save btn');
+  
+                  /** Close this popup. Close will always close the current visible one, if one is visible */
+                  Popup.close();
+              }
+          }]
+      }
+  });
   }
 
   render(){
