f3a1df73ffdc8f7fd0eb161be390de89562ee92f
Tony Yoon <tooonnyy@gmail.com>
2017-11-27 12:16:53 -0500

    lobby/nav-frontend-complete

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -11,45 +11,32 @@ export default class Lobby extends Component{
         return (
             <div id="lobby-main">
 
-              <div id="train-div">
-                        <Link to="/train">
-                            <div className="train-body card-body">
-                                <img className="card-body-img" src="http://ray-bansunglasses.us/wp-content/uploads/2017/08/games-1.png"/>
-                            </div>
-                            <div className="train-header card-header">
-                                <h1>Train</h1>
-                                <p>Training through Missions</p>
-                            </div>
-
-                        </Link>
+              <div id="train-div" className="card">
+                <Link to="/train">
+                    <div className="card-header">
+                        <h1>TRAIN</h1>
+                        <p>Build your skills through Missions</p>
+                    </div>
+                    <div className="card-body">
+                        <img className="card-body-img" src="training-icon.png"/>
                     </div>
 
+                </Link>
+              </div>
 
-                    <div id="battle-div">
-                        <Link to="/battle">
-                            <div className="battle-body card-body">
-                                <img className="card-body-img" src="http://ray-bansunglasses.us/wp-content/uploads/2017/08/games-1.png"/>
-                            </div>
-                            <div className="battle-header card-header">
-                                <h1>Battle</h1>
-                                <p>Battle Opponents For Glory!</p>
-                            </div>
-                        </Link>
+
+            <div id="battle-div" className="card">
+                <Link to="/battle">
+                    <div className="card-header">
+                        <h1>BATTLE</h1>
+                        <p>Battle Opponents For Glory!</p>
                     </div>
-
-                    <div id="sandbox-div">
-                        <Link to="/editor">
-                            <div className="sandbox-body card-body">
-                                <img className="card-body-img" src="http://ray-bansunglasses.us/wp-content/uploads/2017/08/games-1.png"/>
-                            </div>
-                            <div className="sandbox-header card-header">
-                                <h1>Sandbox</h1>
-                                <p>Your Personal Code Editor</p>
-                            </div>
-
-                        </Link>
+                    <div className="card-body">
+                        <img className="card-body-img" src="/battle-icon.png"/>
                     </div>
+                </Link>
             </div>
+        </div>
         )
     }
 }
