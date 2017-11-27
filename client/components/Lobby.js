df435cc5c32a01c80862a3b866d18ba430225872
Tony Yoon <tooonnyy@gmail.com>
2017-11-27 14:57:28 -0500

    card background-color-blue-on-hover

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 // import PopUp from './pop_up'
 
-export default class Lobby extends Component{
+export class Lobby extends Component{
     constructor(){
         super();
     }
