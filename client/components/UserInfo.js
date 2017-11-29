d7139c7b203734e5f93d4ac2bc55c6ff23be2963
Nadim Uddin <nadim.uddin1995@gmail.com>
2017-11-29 13:03:42 -0500

    working more on user profile

--- client/components/UserInfo.js
+++ client/components/UserInfo.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import {connect} from 'react-redux'
+import {List, ListItem} from 'material-ui/List';
 
 class UserInfo extends Component {
-
+    
     render(props) {
+        let listItemStyles={
+            
+                }
         const {email} = this.props;
         const {user} = this.props;
-        return (<div>
-            Name: {user.name}
-            <br />
-            Email: {email}
-            <br />
-        </div>)
+        return (<div id="userProfile">
+                    <div id="userProfile-image">
+                        <img src={user.profileImage} id="profileImage" />
+                    </div>
+                    <div id="userProfile-info">
+                    <List>
+                        <ListItem primaryText="Inbox" style={listItemStyles}/>
+                        <ListItem primaryText="Starred" />
+                        <ListItem primaryText="Sent mail" />
+                        <ListItem primaryText="Drafts" />
+                        <ListItem primaryText="Inbox" />
+                    </List>
+                    </div>
+                </div>)
     }
 }
 
