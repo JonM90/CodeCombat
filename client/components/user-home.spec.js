8fec16f1b8bdae184b24cee9bc195903b81b4a12
Amadou Jallow <wuri1@live.com>
2017-11-30 12:24:42 -0500

    working on testing

--- client/components/user-home.spec.js
+++ client/components/user-home.spec.js
@@ -7,30 +7,39 @@ import Adapter from 'enzyme-adapter-react-16'
 import {UserHome} from './user-home'
 
 import { Train } from './Train';
+import { CodeEditor } from './editor';
 
 const adapter = new Adapter()
 enzyme.configure({adapter})
 
-describe('UserHome', () => {
-  let userHome
+// describe('UserHome', () => {
+//   let userHome
 
-  beforeEach(() => {
-    userHome = shallow(<UserHome email={'cody@email.com'} />)
-  })
+//   beforeEach(() => {
+//     userHome = shallow(<UserHome email={'cody@email.com'} />)
+//   })
 
 //   it('renders the email in an h3', () => {
 //     expect(userHome.find('h3').text()).to.be.equal('Welcome, cody@email.com')
 //   })
-})
+
+//Editor specs
+// describe('<Editor /> Component', () => {
+  
+//     let currentProblem = {title: 'Diff', level: 1, description: 'Return the difference of two numbers', solution: 'function diff(a,b){ return a-b} ', testSpecs: ['assert.equal(diff(1,2), -1)', 'assert.equal(diff(5,2), 3)'], authorId: 2, signature:'diff(a, b)'}
+//     let editor
+     
+//         beforeEach('Create Component and event spy', () => {
+//          editor = shallow(<CodeEditor  currentProblem={ currentProblem } />)
+//         })
+  
+//         it('It has an initial state for currentProblem', () => {
+//             expect(editor.state()).to.be.deep.equal({currentProblem})
+//         })
+//   })
 
 
-//Train specs
-// describe('Train', () => {
-//       beforeEach('Create Component and event spy', () => {
-//         Train = shallow(<Train />)
-//       })
+//});
 
-//       it('', () => {
 
-//       })
-// })
+
