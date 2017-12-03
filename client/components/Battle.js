206ef02b14d2d71ebacb05f1ae90304bdc72b5b2
Tony Yoon <tooonnyy@gmail.com>
2017-12-03 14:46:18 -0500

    final ui ready

--- client/components/Battle.js
+++ client/components/Battle.js
@@ -105,7 +105,7 @@ export class Battle extends Component{
           {!this.state.matchBtn?(
             <div id="battle-lobby">
               <h1>Battle Opponents</h1>
-              <h3>Fight To The Death!</h3>
+              <h3>Win and Take Points From Your Opponents</h3>
               <button onClick={this.handleMatch}>Find Match</button>
             </div>
             ):null
